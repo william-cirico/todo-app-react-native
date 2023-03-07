ab9b2976580792b42226c89a72908309fb928720
William <contato.williamc@gmail.com>
2023-03-07 18:14:24 -0300

    atual

--- src/components/controllers/DatePicker.tsx
+++ src/components/controllers/DatePicker.tsx
@@ -1,26 +1,70 @@
 import { useState } from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { Platform, StyleSheet, Text, TouchableOpacity } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import dayjs from "dayjs";
 
 export function DatePicker() {
-    const [showDate, setShowDate] = useState(false);
-    const [showTime, setShowTime] = useState(false);
     const [date, setDate] = useState(new Date());
+    const [mode, setMode] = useState<"date" | "time">("date");
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setShow(false);
+        setDate(currentDate);
+
+        if (mode === "date") {
+            showTimepicker();
+        }
+    };
+
+    const showMode = (currentMode) => {
+        if (Platform.OS === 'android') {
+            setShow(false);
+        }
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    }
 
     return (
         <>
-            <TouchableOpacity onPress={() => setShowDate(true)}>
-                <Text>Abrir DatePicker</Text>
+            <TouchableOpacity style={styles.dateViewer} onPress={showDatepicker}>
+                <Text style={styles.dateViewerText}>{dayjs(date).format("D [de] MMMM - HH:mm")}</Text>
             </TouchableOpacity>
-            {
-                showDate &&
+            {show && (
                 <DateTimePicker
-                    onChange={() => setShowTime(true)}
-                    mode="date"
-                    display="default"
-                    value={new Date()}
+                    testID="dateTimePicker"
+                    value={date}
+                    mode={mode}
+                    is24Hour={true}
+                    onChange={onChange}
                 />
-            }
+            )}
         </>
     );
 }
+
+const styles = StyleSheet.create({
+    dateViewer: {
+        backgroundColor: "#f5f5f5",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingVertical: 15,
+        paddingHorizontal: 12,
+        borderWidth: 1,
+        borderColor: "#666",
+        borderRadius: 4,
+        marginVertical: 20
+    },
+    dateViewerText: {
+        color: "#fff"
+    }
+});
